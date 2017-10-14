252da469164a33f962f72d745cde0bec01d5ce93
Dave Barthly <davebarthly@gmail.com>
2017-10-14 01:07:48 -0400

    Grab JSON to files on repo

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const axios = require("axios");
 
-console.log(process.env.TOKEN);
+var github = require("octonode");
+
+var client = github.client();
+
+var ghrepo = client.repo("markerikson/react-redux-links");
+
+ghrepo.contents("", "master", function(err, data, headers) {
+  let filteredRepoFiles = data.slice(2, data.length);
+});
 
 const PORT = 4000;
 app.listen(PORT);
