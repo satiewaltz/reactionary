05e801ac3f8b7d43f85eef2080662c6c1724850f
Dave Barthly <davebarthly@gmail.com>
2017-10-17 03:22:55 -0400

    Complete data compute on AST and make code more functional

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const express = require("express");
 const app = express();
 const axios = require("axios");
 require("./axios.config.js");
-const parse = require("markdown-to-ast").parse;
+const remarkAbstract = require("remark");
+const remark = remarkAbstract();
 
 const url =
   "https://api.github.com/repos/markerikson/react-redux-links/contents";
@@ -13,6 +14,23 @@ const logError = err => console.log(err.response);
 const getData = async url =>
   (await axios.get(url).catch(logError)).data;
 
+const computeAST = AST => {
+  const lists = AST.children
+    .filter(el => el.type !== "heading")
+    .map(({ children }) => ({ list: children }));
+
+  const headings = AST.children
+    .filter(el => el.type === "heading" && el.depth >= 4)
+    .map(({ children }) => ({ heading: children[0].value }));
+
+  const output = headings.map((el, i) => ({
+    ...el,
+    list: lists[i].list
+  }));
+  console.log(output);
+  return output;
+};
+
 async function main(url) {
   const repoData = await getData(url);
   const filterRepo = repoData
@@ -20,8 +38,9 @@ async function main(url) {
     .map(obj => obj.download_url);
 
   const fileData = await getData(filterRepo[3]);
-  const AST = parse(fileData);
-  console.log(AST);
+  const AST = remark.parse(fileData);
+
+  return computeAST(AST);
 }
 
 main(url);
