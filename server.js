95ccd3334153f4b9c947adcb4b6b739c1ba3ae11
Dave Barthly <davebarthly@gmail.com>
2017-10-16 21:29:11 -0400

    Add markdown AST parser to generalize data

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+const fs = require("fs");
 require("dotenv").config();
 const express = require("express");
 const app = express();
 const axios = require("axios");
-const fs = require("fs");
 require("./axios.config.js");
+const md = require("markdown-it")({ html: false });
+const mdAST = require("./markdownAST.js");
 
 const url =
   "https://api.github.com/repos/markerikson/react-redux-links/contents";
@@ -14,14 +16,17 @@ const getData = async url =>
 
 async function main(url) {
   const repoData = await getData(url);
-  const filtered = repoData
+  const filterRepo = repoData
     .slice(2, repoData.length)
     .map(obj => obj.download_url);
 
-  const fileData = await getData(filtered[3]);
+  const fileData = await getData(filterRepo[3]);
 
-  console.log(fileData);
-  return filtered;
+  const tokens = md.parse(fileData);
+  const mdJSON = mdAST.makeAST(tokens);
+  // const filterAST = mdJSON.map(obj =>)
+  // dJSON[0].children[0].content
+  // console.log(mdJSON);
 }
 
 main(url);
