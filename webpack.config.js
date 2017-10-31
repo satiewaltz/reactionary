a68a817aba67273794a3c5ad78ed18769a0533b1
Dave Barthly <davebarthly@gmail.com>
2017-10-31 04:37:58 -0400

    Setup local function invocation

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 const nodeExternals = require("webpack-node-externals");
 const StartServerPlugin = require("start-server-webpack-plugin");
+const slsw = require("serverless-webpack");
 
 const isDev = process.env.NODE_ENV == "dev";
 
@@ -12,23 +13,14 @@ module.exports = {
       importType: "commonjs",
       modulesDir: path.resolve("./node_modules")
     })
-  ], // in order to ignore all modules in node_modules folder
+  ],
 
-  context: path.resolve(__dirname, "src"),
-  entry: "./index.mjs",
-  plugins: getPlugins(),
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    libraryTarget: "commonjs",
-    pathinfo: isDev,
-    publicPath: "/mem/"
-  },
+  entry: slsw.lib.entries,
+  plugins: [ new webpack.NamedModulesPlugin() ],
 
   //////////////////////////////////////////////////////
   // Resolves imports - helps make importing less verbose
   resolve: {
-    mainFiles: [ "index", "module", "main" ],
     extensions: [ ".js", ".mjs" ], // resolves imports
     modules: [
       path.resolve(__dirname, "./src"),
@@ -48,41 +40,3 @@ module.exports = {
     ]
   }
 };
-
-function getPlugins(plugins = []) {
-  if (isDev) {
-    plugins = plugins.concat([ new webpack.NamedModulesPlugin() ]);
-  }
-
-  return plugins;
-}
-
-// function getEntries(entries = []) {
-//   if (isDev) {
-//     entries = entries.concat([
-//       "webpack-dev-server/client?http://0.0.0.0:8080",
-//       "webpack/hot/only-dev-server",
-//       "./index.mjs"
-//     ]);
-//   } else {
-//     entries = "./index.mjs";
-//   }
-
-//   return entries;
-// }
-
-// function initDevServer(devConfig = {}) {
-//   if (isDev) {
-//     devConfig = {
-//       host: "0.0.0.0",
-//       hot: true,
-//       inline: true,
-//       compress: true,
-//       historyApiFallback: true,
-//       disableHostCheck: true,
-//       publicPath: "/dist/"
-//     };
-//   }
-
-//   return devConfig;
-// }
